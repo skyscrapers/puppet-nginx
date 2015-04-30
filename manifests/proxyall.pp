237a0383fd253d76696c51023b153a07712cc341
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2015-04-30 11:39:19 +0200

    added license and renamed redirect

--- manifests/proxyall.pp
+++ manifests/proxyall.pp
@@ -1,8 +1,24 @@
-## DOCUMENTATION
+##### LICENSE
+
+# Copyright (c) Skyscrapers (iLibris bvba) 2015 - http://skyscraper.eu
 #
-# generate redirect vhost for nginx
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-##
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# == Class nginx::proxyall
+#
+# This class will proxy all requests on a port to a target
+#
 define nginx::proxyall (
     $port,
     $target,
@@ -11,7 +27,7 @@ define nginx::proxyall (
   file {
     "/etc/nginx/sites-available/${name}.conf":
       path    => "/etc/nginx/sites-available/${name}.conf",
-      content => template('nginx/etc/nginx/sites-available/proxy-all.conf.erb'),
+      content => template('nginx/etc/nginx/sites-available/proxyall.conf.erb'),
       mode    => '0644',
       owner   => root,
       group   => root,
