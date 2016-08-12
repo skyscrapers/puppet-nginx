62686caa8c3a5accf77866e62532d0cf8ab23c31
Mattias Gees <mattias.gees@gmail.com>
2016-08-12 08:50:02 +0200

    Add logpath of static vhost

--- manifests/static.pp
+++ manifests/static.pp
@@ -21,7 +21,8 @@
 #
 define nginx::static (
   $root_path                  = undef,
-  $option_paths                = undef,
+  $log_path                   = undef,
+  $option_paths               = undef,
   $server_aliases             = [],
   $port                       = 80,
   $ip                         = '',
