1f463ebb8a33150cd8bf671b2bad545ec37f8800
iuri aranda <iuri.aranda@gmail.com>
2016-09-22 18:32:07 +0200

    Added access-control-allow-origin CORS header to redirects

--- manifests/redirect.pp
+++ manifests/redirect.pp
@@ -58,6 +58,7 @@ define nginx::redirect (
     $ssl_certificate = undef,
     $ssl_certificate_key = undef,
     $ssl_dh_param = undef,
+    $access_control_allow_origin = undef,
   ) {
 
   if($ssl_certificate == undef or $ssl_certificate_key == undef){
